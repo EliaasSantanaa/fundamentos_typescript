9ac77aeb5c773e5cff1f4c5fc63c8ba450a7ef75
Eliaas00 <eliaassantana00@gmail.com>
2025-09-23 21:26:25 -0300

    feat: type number

---
+++ src/types/type_number.ts
@@ -0,0 +1,11 @@
+// Tipo Number - sem erro
+let valor: number
+valor = 15
+console.log(valor)
+
+// Tipo Number - com erro
+let idade: number
+
+// idade = "25" - erro
+idade = 30
+console.log(idade)
